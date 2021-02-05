6c9ba04d8a861bde33c3eb062287ed281125f351
Bartosz Nocun <bartosznocun1@gmail.com>
2021-02-05 23:09:05 +0100

    Add Task1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 /* Here goes your JS code */
+const btnShow = document.querySelector('#show-popup-form');
+const btnClose = document.querySelector('#close-popup-form');
+const popup = document.querySelector('.popup');
+
+btnShow.addEventListener('click', function () {
+    popup.classList.add('popup--active');
+    btnShow.classList.add('btn-main--hide');
+});
+
+btnClose.addEventListener('click', function () {
+    popup.classList.remove('popup--active');
+    btnShow.classList.remove('btn-main--hide');
+});
